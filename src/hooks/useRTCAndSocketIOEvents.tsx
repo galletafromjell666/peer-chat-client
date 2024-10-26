a91d455c1c96db3d327f55ff6ad13bf497ecbb57
Giovanni Aguirre <gaguirre@2600hz.com>
2024-10-26 17:46:41 -0600

    Add socketIO event listeners

--- src/hooks/useRTCAndSocketIOEvents.tsx
+++ src/hooks/useRTCAndSocketIOEvents.tsx
@@ -7,10 +7,27 @@ function useRTCAndSocketIOEvents() {
   useEffect(() => {
     if (!socketIOClient) return;
     console.log("BackgroundEvents init!");
+
+    // init
     const handleInitEvent = (data: unknown) => {
       console.log("Received init", data);
     };
-    socketIOClient!.subscribe("init", handleInitEvent);
+
+    socketIOClient.subscribe("init", handleInitEvent);
+
+    // receive candidate
+    const handleReceiveCandidate = (data: unknown) => {
+      console.log("Received ICE candidate", data);
+    };
+
+    socketIOClient.subscribe("receive-candidate", handleReceiveCandidate);
+
+    // receive answer
+    const handleReceiveAnswer = (data: unknown) => {
+      console.log("Received answer", data);
+    };
+    socketIOClient.subscribe("receive-answer", handleReceiveAnswer);
+
     return () => {};
   }, []);
 
